97dccbd509436d4c529f2da64430d5a2f1be087f
Stepan Golovanov <vilkelis@yandex.ru>
2020-05-02 01:47:28 +0500

    Cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import natash_img from './natash.jpg'
+import Card from './components/Card';
 
 function App() {
+
+  const firstCardClick = (obj) => {
+    console.log('First card click');
+  };
+
+  const secondCardClick = (obj) => {
+    console.log('Уже встаю... Встаю!');
+  }
+
+
+  const cards = [ {title:'Card title', 
+                 text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+                 buttonCaption: 'Do something',
+                 onButtonClick: firstCardClick},
+                {title: 'Наташ! Вставай!',
+                 text: 'Мы все уронили! Вообще все!',
+                 buttonCaption: 'Встаю!',
+                 onButtonClick: secondCardClick}];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="card-list">
+      <Card {...cards[0]}/>
+      <Card {...cards[1]}>
+        <img src={natash_img} className="card-img-top" alt="Наташ! Вставай! Мы все уронили..." />        
+      </Card>
+      </div>
+    </React.Fragment>
   );
 }
 
